6530356cd06be9eb41b42b44ce7c49a231ec2cd1
Dmytro Cheshun <dmytro.cheshun@gmail.com>
2015-08-12 12:18:02 +0300

    Added smooth animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,85 @@
-$(window).resize(function(){
+$(window).resize(function() {
 	$('.center-block').each(function() {
 		$(this).css({
-			position:'relative',
-			left: ($(this).parent().width() - $(this).outerWidth())/2,
-			top: ($(this).parent().height() - $(this).outerHeight())/2
-		});
+				position: 'relative'
+			})
+			.animate({
+				left: ($(this).parent().width() - $(this).outerWidth()) / 2,
+				top: ($(this).parent().height() - $(this).outerHeight()) / 2,
+				opacity: 1
+			}, {
+				duration: 300,
+				easing: 'swing'
+			});
 	});
 
 	$('.center-block-horizontal').each(function() {
 		$(this).css({
-			position:'relative',
-			left: ($(this).parent().width() - $(this).outerWidth())/2
-		});
+				position: 'relative'
+			})
+			.animate({
+				left: ($(this).parent().width() - $(this).outerWidth()) / 2,
+				opacity: 1
+			}, {
+				duration: 300,
+				easing: 'swing'
+			});
 	});
 
 	$('.carousel-block-horizontal').each(function() {
 		$(this).css({
-			position:'relative',
-			left: ($(this).parent().width() - $(this).outerWidth())/2
-		});
+				position: 'relative'
+			})
+			.animate({
+				left: ($(this).parent().width() - $(this).outerWidth()) / 2,
+				opacity: 1
+			}, {
+				duration: 300,
+				easing: 'swing'
+			});
 
+	});
+
+	console.log($('.left-block').outerHeight());
+	console.log($(window).height());
+
+	if ($('.left-block').outerHeight() + $('header').outerHeight() > $(window).height()) {
+		$('.left-block').css({
+			position: 'absolute'
+		});
+	} else {
+		$('.left-block').css({
+			position: 'fixed'
+		});
+	}
+});
+
+$('#next-game-carousel').on('slid.bs.carousel', function() {
+	$('.carousel-block-horizontal').each(function() {
+		$(this).css({
+				position: 'relative'
+			})
+			.animate({
+				left: ($(this).parent().width() - $(this).outerWidth()) / 2,
+				opacity: 1
+			}, {
+				duration: 300,
+				easing: 'swing'
+			});
 	});
 });
 
-$('#next-game-carousel').on('slid.bs.carousel', function () {
-  $('.carousel-block-horizontal').each(function() {
+$('#accordion').on('shown.bs.collapse', function() {
+	$('.center-block-horizontal').each(function() {
 		$(this).css({
-			position:'relative',
-			left: ($(this).parent().width() - $(this).outerWidth())/2
-		});
-	});
-});
-
-$('#accordion').on('shown.bs.collapse', function () {
-  $('.center-block-horizontal').each(function() {
-		$(this).css({
-			position:'relative',
-			left: ($(this).parent().width() - $(this).outerWidth())/2
-		});
+				position: 'relative'
+			})
+			.animate({
+				left: ($(this).parent().width() - $(this).outerWidth()) / 2
+			}, {
+				duration: 300,
+				easing: 'swing'
+			});
 	});
 })
 
@@ -45,6 +87,6 @@ $('#accordion').on('shown.bs.collapse', function () {
 $(document).ready(function() {
 	$(window).resize();
 	$('#next-game-carousel').carousel({
-      interval: 10000
-    })
+		interval: 10000
+	})
 });
